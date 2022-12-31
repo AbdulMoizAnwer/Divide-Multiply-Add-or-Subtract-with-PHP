9181970935b83be2532421b50b813aa438934b20
Abdul Moiz Anwer <89318559+AbdulMoizAnwer@users.noreply.github.com>
2022-12-31 18:22:38 +0500

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title> 
+</head>
+<body>
+    
+<form action="backend2.php" method="POST">
+    <label>Enter First Number</label>
+    <input type="number" name="n1"><br>
+    <label>Enter Second Number</label>
+    <input type="number" name="n2"><br>
+    <input type="submit" name="Sum" value="Sum">
+    <input type="submit" name="min" value="Difference">
+    <input type="submit" name="Mult" value="Multiply">
+    <input type="submit" name="Div" value="Divide">
+</form>
+</body>
+</html>
